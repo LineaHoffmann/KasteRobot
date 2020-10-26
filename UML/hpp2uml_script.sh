41f9bc7d2b1406a36dff9da980bd3843609e8586
PoSRP <soerenriisom@gmail.com>
2020-10-26 16:07:20 +0100

    Fix in UML script
    Didn't run when in UML sub-dir. 
    Now pipes direct to .png

--- UML/hpp2uml_script.sh
+++ UML/hpp2uml_script.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 # Script to generate PlantUML file for C++ projects
 # Recursively looks through the current directory
+# Written by Søren Pedersen
 
-echo "Trying to build UML diagrams from source folder .. "
+echo "Trying to build UML diagrams recursively from parent folder .. "
 
-FILES=$(find -name "*.hpp" | grep ".hpp")
-NUM_FILES=$(ls -R | grep -c ".hpp")
+PARENT_DIR="$(dirname "$PWD")"
+echo "Parent folder: $PARENT_DIR"
+
+PLANTUML_SRC_PATH=$(find /usr -name "plantuml.jar")
+
+FILES=$(find $PARENT_DIR -name "*.hpp" | grep ".hpp")
+NUM_FILES=$(ls -R $PARENT_DIR | grep -c ".hpp")
+
+if [ $NUM_FILES -eq 0 ]; then
+	echo "No files found, stopping .. "
+	exit
+fi
 
 echo "Found $NUM_FILES files: "
 echo $FILES
@@ -18,3 +29,8 @@ done
 
 echo "Running hpp2plantuml with above files as input arguments .. "
 hpp2plantuml $PUML_STRING -o plantuml_output.puml
+
+echo "Running PlantUML on generated .puml file .."
+cat plantuml_output.puml | java -jar $PLANTUML_SRC_PATH -pipe > uml_diagram.png
+
+echo "Complete. I've probably made a png file for you."
