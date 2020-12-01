91adddb7a5b1b2268ec8d6fa0deb542b32c1f06e
LineaHoffmann <32227865+LineaHoffmann@users.noreply.github.com>
2020-12-01 11:21:25 +0100

    Updated script

--- kasteRobotSqlScript.sql
+++ kasteRobotSqlScript.sql
@@ -1,15 +1,5 @@
 CREATE DATABASE kasteRobot;
 USE kasteRobot;
-CREATE TABLE IF NOT EXISTS throw (
-throwID INT AUTO_INCREMENT PRIMARY KEY,
-successful BOOLEAN,
-deviation DOUBLE,
-xy_plane_angle DOUBLE,
-cz_plane_angle DOUBLE,
-release_velocity DOUBLE,
-tcp_velocity_cal DOUBLE,
-tcp_velocity_act DOUBLE
-);
 CREATE TABLE IF NOT EXISTS position (
 positionID INT AUTO_INCREMENT PRIMARY KEY,
 x_pos DOUBLE,
@@ -27,4 +17,16 @@ start_positionID INT,
 stop_positionID INT,
 FOREIGN KEY (start_positionID) REFERENCES kasteRobot.position(positionID),
 FOREIGN KEY (stop_positionID) REFERENCES kasteRobot.position(positionID)
+);
+CREATE TABLE IF NOT EXISTS throw (
+throwID INT AUTO_INCREMENT PRIMARY KEY,
+created_at VARCHAR(30),
+FOREIGN KEY (created_at) REFERENCES kasteRobot.log(created_at),
+successful BOOLEAN,
+deviation DOUBLE,
+xy_plane_angle DOUBLE,
+cz_plane_angle DOUBLE,
+release_velocity DOUBLE,
+tcp_velocity_cal DOUBLE,
+tcp_velocity_act DOUBLE
 );
