9e8d8ff96e874543b4afad605afd3ca87547b2d6
LineaHoffmann <32227865+LineaHoffmann@users.noreply.github.com>
2020-12-02 08:47:19 +0100

    Updateret Script

--- kasteRobotSqlScript.sql
+++ kasteRobotSqlScript.sql
@@ -1,3 +1,4 @@
+DROP DATABASE kasteRobot;
 CREATE DATABASE kasteRobot;
 USE kasteRobot;
 CREATE TABLE IF NOT EXISTS position (
@@ -9,19 +10,23 @@ x_rotation DOUBLE,
 y_rotation DOUBLE,
 z_rotation DOUBLE
 );
+CREATE TABLE IF NOT EXISTS moveEntry(
+moveEntry_ID INT AUTO_INCREMENT PRIMARY KEY,
+movetype VARCHAR(30),
+a_positionID INT,
+b_positionID INT,
+FOREIGN KEY (a_positionID) REFERENCES kasteRobot.position(positionID),
+FOREIGN KEY (b_positionID) REFERENCES kasteRobot.position(positionID)
+);
 CREATE TABLE IF NOT EXISTS log (
-created_at VARCHAR(30) PRIMARY KEY,
-logType VARCHAR(20),
-moveType VARCHAR(50),
-start_positionID INT,
-stop_positionID INT,
-FOREIGN KEY (start_positionID) REFERENCES kasteRobot.position(positionID),
-FOREIGN KEY (stop_positionID) REFERENCES kasteRobot.position(positionID)
+log_ID INT AUTO_INCREMENT PRIMARY KEY,
+created_at VARCHAR(30),
+entryType VARCHAR(50)
 );
 CREATE TABLE IF NOT EXISTS throw (
 throwID INT AUTO_INCREMENT PRIMARY KEY,
-created_at VARCHAR(30),
-FOREIGN KEY (created_at) REFERENCES kasteRobot.log(created_at),
+log_ID INT,
+FOREIGN KEY (log_ID) REFERENCES kasteRobot.log(log_ID),
 successful BOOLEAN,
 deviation DOUBLE,
 xy_plane_angle DOUBLE,
