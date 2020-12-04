b476d5a785ad823099a4f67afde820b7b41e6800
JW-ubnt 1804 <jawar19@student.sdu.dk>
2020-12-04 02:19:57 -0800

    1st not working

--- src/logic/xcontroller.cpp
+++ src/logic/xcontroller.cpp
@@ -270,16 +270,34 @@ void xController::testDetectAndPickUp2(std::shared_ptr<ximageHandler> mImagehand
 
 void xController::testThrowSpeedJ(double angle)
 {
+    int i{0};
     RobotData robotData(mRobot->getURStruct());
     int prevPollingRate = mRobot->getPollingRate();
+
+    std::vector<std::vector<double> > startq{{0, -1.39626,-2.79253,0,1.57,1.57}};
+    mRobot->setMove(ROBOT_MOVE_TYPE::MOVE_JLIN,startq, 1, 1);
+    while(mRobot->getIsBusy()){
+        std::this_thread::sleep_for(std::chrono::milliseconds(10));
+        robotData = mRobot->getURStruct();
+        std::cout << robotData.robotJointPosition[2] << " | " << angle << std::endl;
+    }
     mRobot->setPollingRate(125);
+
+    std::cout << " 1: " << robotData.robotJointPosition[2] << " | " << angle << std::endl;
+
     mRobot->setMove(ROBOT_MOVE_TYPE::SPEEDJ);
 
     while (mRobot->getIsBusy()){
+        ++i;
+        robotData = mRobot->getURStruct();
+        std::cout << robotData.robotJointPosition[2] << " | " << angle << std::endl;
         if (robotData.robotJointPosition[2] >= angle){
             mGripper->release();
+            std::cout << i << std::endl;
+            break;
         }
     }
+    std::cout << robotData.robotJointPosition[2] << " | " << angle << std::endl;
 
 
     mRobot->setPollingRate(prevPollingRate);
